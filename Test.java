98b48258c12e5da009eb091393991a4558c3b547
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-06 08:47:07 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        //print
+        /*
+        September 6, 2024
+        First Programming Session
+        */
+        System.out.println ("Anna \t\tRhodora \nQuitaleg ");
+        System.out.print ("\n\n\nAnna\"s Class ");
+        System.out.println ("I love Java programming ");
+    }    
+}
