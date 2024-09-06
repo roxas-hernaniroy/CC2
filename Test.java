b03e75e5468169035994af4a912874d1a7f6432c
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-06 20:11:59 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,13 +1,23 @@
 public class Test {
     public static void main(String[] args) {
+        
         //single line comment
+        
         /*
-        multi
-        line
+        multi line
         comment
         */
-        System.out.println ("Anna \t\tRhodora \nQuitaleg "); //println adds a line break after the line; \t adds tabspace before the text; \n adds a new line before the text
-        System.out.print ("\n\n\nAnna\"s Class ");
-        System.out.println ("I love Java programming ");
+        
+        //never forget the semicolon
+        //text should always be between "", add a space after text string for readability
+        
+        System.out.println ("Sator \tArepo \nTenet \@Opera");
+        // println adds a line break after the code line
+        // \t adds tabspace before the text
+        // \n adds a new line before the text
+        // \ followed by the symbol that would otherwise break code prevents it from doing so
+        
+        System.out.print ("I love Java programming ");
+        //print does not add a line break after the code line
     }    
 }
