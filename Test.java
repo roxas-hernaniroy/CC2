7dc6b438201c4e2bce9407850e09ef32928e6831
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-07 09:07:58 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -11,11 +11,11 @@ public class Test { //public class [filename]
         //never forget the semicolon ;
         //text should always be between "", add a space after text string for readability
         
-        System.out.println ("Sator \tArepo \nTenet \@Opera");
+        System.out.println ("Sator \tArepo \nTenet \"Opera ");
         // println adds a line break after the code line
         // \t adds tabspace before the text
         // \n adds a new line before the text
-        // \ followed by the symbol that would otherwise break code prevents it from doing so
+        // \ followed by the symbol that would otherwise confuse code prevents it from doing so
         
         System.out.print ("Sator Arepo Tenet ");
         System.out.print ("Opera Rotas ");
