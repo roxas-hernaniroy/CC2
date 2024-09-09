80559e605de7ab09fce5794cc73d230258f8d891
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-09 10:24:09 +0800

    Update scannertest.java

--- scannertest.java
+++ scannertest.java
@@ -5,12 +5,17 @@ public class scannertest {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter input:");
 		
-		//String text = input.nextLine();
-		//int number = input.nextInt();
+		String text = input.nextLine();
+		int number = input.nextInt();
 		double floatnumber = input.nextDouble();
 		
 		//System.out.println("text: " + text);
 		//System.out.println("number: " + number);
 		System.out.println("floating point number : " + floatnumber);
+		
+		/*not sure about this code block here
+		if nextLine() = "STOP";
+		close();
+		*/
 	}
 }
