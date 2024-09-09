a7ed5acf8d404b9c631c8c6e2a581836e13fad0b
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-09 08:05:58 +0800

    Create cc2challenge2roxashrb.java

---
+++ cc2challenge2roxashrb.java
@@ -0,0 +1,11 @@
+public class cc2challenge2roxashrb {
+    public static void main(String[] args) {
+        //set variables
+        int L = ;
+        int W = ;
+        
+        //print result
+        System.out.println("A");
+        System.out.println("P");
+    }
+}
