c0eebb77254fe6b45c7b31643fe648e9c6d8f7b9
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-09 08:04:03 +0800

    Update cc2module2.java

--- cc2module2.java
+++ cc2module2.java
@@ -1,5 +1,11 @@
 public class cc2module2 {
     public static void main(String[] args) {
-    
+        //set variables
+        int L = ;
+        int W = ;
+        
+        //print result
+        System.out.println("A");
+        System.out.println("P");
     }
 }
