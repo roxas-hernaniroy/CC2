268157a40d6c267bcdd576da4bbf47ce636dcc53
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-19 09:35:04 +0800

    Update RadixConv.java

--- RadixConv.java
+++ RadixConv.java
@@ -1,6 +1,41 @@
 import java.util.Scanner;
 public class RadixConv {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        boolean continueProgram = true;
+        
+        while (continueProgram) {
+            System.out.println("Enter input:");
+            long numberInput = userInput.nextLong();
+            String textInput = userInput.nextLine();
+        
+            System.out.println("Enter desired base:");
+            byte radix;
+            radix = userInput.nextByte();
+        
+            switch (radix) {
+                case 16:
+                    String decimalToHex = Long.toString(numberInput, 16);
+                    System.out.println("Hexadecimal Result: " + decimalToHex);
+                    break;
+                case 8:
+                    String decimalToOctal = Long.toString(numberInput, 8);
+                    System.out.println("Octal Result: " + decimalToOctal);
+                    break;
+                case 2:
+                    String decimalToBinary = Long.toString(numberInput, 2);
+                    System.out.println("Binary Result: " + decimalToBinary);
+                    break;
+                default:
+                    System.out.println("Work in progress, choose between 2/8/16");
+            }
+        }
+    }
+}
+
+/*import java.util.Scanner;
+public class RadixConv {
+    public static void main(String[] args) {
         
         Scanner userInput = new Scanner(System.in);
         
@@ -31,6 +66,7 @@ public class RadixConv {
         }
     }
 }
+*/
 
 /*
 import java.util.Scanner;
