c807edc461555d6797bcb51bb6f47c6f63290510
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-20 23:09:11 +0800

    Update RadixConverter.java

--- RadixConverter.java
+++ RadixConverter.java
@@ -47,3 +47,59 @@ public class RadixConverter {
         }
     }
 }
+
+//stylised version
+/*
+import java.util.Scanner;
+public class RadixConverter {
+    public static void main(String[] args) {
+        
+        Scanner userInput = new Scanner(System.in);
+        int programState = 1;
+        
+        while (programState < 2) {
+            System.out.println("\nEnter input:");
+            
+            if (!userInput.hasNextLong()) {
+                String textInput = userInput.nextLine();
+                switch (textInput) {
+                    case "STOP":
+                        programState++;
+                        break;
+                    default:
+                        System.out.println("\nType STOP to close program.");
+                        break;
+                }
+            } else {
+                long numberInput = userInput.nextLong();
+            
+                System.out.println("\nEnter desired base:");
+                int radix = userInput.nextInt();
+        
+                switch (radix) {
+                    case 16:
+                        String decimalToHex = Long.toString(numberInput, 16);
+                        System.out.println("\nHexadecimal Result: " + decimalToHex);
+                        break;
+                    case 8:
+                        String decimalToOctal = Long.toString(numberInput, 8);
+                        System.out.println("\nOctal Result: " + decimalToOctal);
+                        break;
+                    case 2:
+                        String decimalToBinary = Long.toString(numberInput, 2);
+                        System.out.println("\nBinary Result: " + decimalToBinary);
+                        break;
+                    default:
+                        System.out.println("\nChoose between 2, 8, or 16 for now.");
+                }
+            }
+        }
+        try {
+            System.out.println("\nClosing program...");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) { 
+            e.printStackTrace(); 
+        }
+    }
+}
+*/
