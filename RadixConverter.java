e144f0140ced37bdf5fceb55e1dc35b2dea0186f
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-20 23:10:01 +0800

    Update RadixConverter.java

--- RadixConverter.java
+++ RadixConverter.java
@@ -8,40 +8,40 @@ public class RadixConverter {
         int programState = 1;
         
         while (programState < 2) {
-            System.out.println("Enter input:");
+            System.out.println("\nEnter input:");
             
             if (!userInput.hasNextLong()) {
                 String textInput = userInput.nextLine();
                 switch (textInput) {
                     case "STOP":
-                        System.out.println("...\n...\n...\nClosing program...");
+                        System.out.println("\n...Closing program...");
                         programState++;
                         break;
                     default:
-                        System.out.println("Type STOP to close program.");
+                        System.out.println("\nType STOP to close program.");
                         break;
                 }
             } else {
                 long numberInput = userInput.nextLong();
             
-                System.out.println("Enter desired base:");
+                System.out.println("\nEnter desired base:");
                 int radix = userInput.nextInt();
         
                 switch (radix) {
                     case 16:
                         String decimalToHex = Long.toString(numberInput, 16);
-                        System.out.println("Hexadecimal Result: " + decimalToHex);
+                        System.out.println("\nHexadecimal Result: " + decimalToHex);
                         break;
                     case 8:
                         String decimalToOctal = Long.toString(numberInput, 8);
-                        System.out.println("Octal Result: " + decimalToOctal);
+                        System.out.println("\nOctal Result: " + decimalToOctal);
                         break;
                     case 2:
                         String decimalToBinary = Long.toString(numberInput, 2);
-                        System.out.println("Binary Result: " + decimalToBinary);
+                        System.out.println("\nBinary Result: " + decimalToBinary);
                         break;
                     default:
-                        System.out.println("Choose between 2, 8, or 16 for now.");
+                        System.out.println("\nChoose between 2, 8, or 16 for now.");
                 }
             }
         }
