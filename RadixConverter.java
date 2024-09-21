030eccf8a492e9ed234be20409ec33c383d30017
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-21 09:45:38 +0800

    Update RadixConverter.java

--- RadixConverter.java
+++ RadixConverter.java
@@ -1,150 +1,113 @@
-//Roxas, Hernani Roy B.
-//CITCS 1B-B
 import java.util.Scanner;
 public class RadixConverter {
-    public static void main(String[] args) {
-        
+public static void main(String[] args) {
+    
         Scanner userInput = new Scanner(System.in);
         byte programState = 1;
-        
         while (programState > 0) {
-            System.out.println("\nEnter input:");
+
+            System.out.println("\nEnter origin radix (2 to 16) or STOP: ");
             
-            if (!userInput.hasNextLong()) {
+            if (!userInput.hasNextInt()) {
                 String textInput = userInput.nextLine();
-                switch (textInput) {
+                switch (textInput){
                     case "STOP":
-                        System.out.println("\n...Closing program...");
+                        System.out.println("\nClosing program");
                         programState--;
                         break;
                     default:
-                        System.out.println("\nType STOP to close program.");
-                        break;
-                }
-            } else {
-                /*
-                System.out.println("Enter origin radix: ")
-                inr originRadix = userInput.nextInt();
-                if (!originRadix.hasNextInt()) {
-                    String originRadixElevenAbove = userInput.nextLine();
-                        switch (originRadixElevenAbove) {
-                            case "11":
-                            case "12":
-                            case "13":
-                            case "14":
-                            case "15":
-                            case "16":
-                        }
-                } else {
-                    switch (originRadix) {
-                        case 2:
-                            
-                        case 3:
-                        case 4:
-                        case 5:
-                        case 6;
-                        case 7:
-                        case 8:
-                        case 9:
-                        case 10:
-                            System.out.println("Input number to be converted: ");
-                            long numberInput = userInput.nextLong();
-                            String baseTenToBaseTwo = Long.toString(numberInput, 2);
-                            String baseTenToBaseThree = Long.toString(numberInput; 3);
-                            String baseTenToBaseFour = Long.toString(numberInput, 4);
-                            String baseTenToBaseFive =
-                            String baseTenToBaseSix =
-                            String baseTenToBaseSeven =
-                            String baseTenToBaseEight =
-                            String baseTenToBaseNine =
-                            String baseTenToBaseEleven =
-                            String baseTenToBaseTwelve =
-                            String baseTenToBaseThirteen =
-                            String baseTenToBaseFourteen =
-                            String baseTenToBaseFifteen =
-                            String baseTenToBaseSixteen =
-                            System.out.println("\n" + baseTenTobaseTwo + "\n" + );
-                    }
-                }
-                */
-                long numberInput = userInput.nextLong();
+                        System.out.println("\nType STOP to close program");
+                }        
+            }
             
-                System.out.println("\nEnter desired base:");
-                int radix = userInput.nextInt();
-        
-                switch (radix) {
-                    case 16:
-                        String decimalToHex = Long.toString(numberInput, 16);
-                        System.out.println("\nHexadecimal Result: " + decimalToHex);
-                        break;
-                    case 8:
-                        String decimalToOctal = Long.toString(numberInput, 8);
-                        System.out.println("\nOctal Result: " + decimalToOctal);
-                        break;
-                    case 2:
-                        String decimalToBinary = Long.toString(numberInput, 2);
-                        System.out.println("\nBinary Result: " + decimalToBinary);
-                        break;
-                    default:
-                        System.out.println("\nChoose between 2, 8, or 16 for now.");
-                }
+            int radixInput = userInput.nextInt();
+            switch (radixInput) {
+                default:
+                    System.out.println("\nTry again, only base2 to base16");
+                case 2:
+                    userInput.useRadix(2);
+                    System.out.println("\nEnter number to be converted: ");
+                    long inputTwo = userInput.nextLong();
+                    String baseTwoTwo = Long.toString(inputTwo, 2);
+                    String baseTwoThree = Long.toString(inputTwo, 3);
+                    String baseTwoFour = Long.toString(inputTwo, 4);
+                    String baseTwoFive = Long.toString(inputTwo, 5);
+                    String baseTwoSix = Long.toString(inputTwo, 6);
+                    String baseTwoSeven = Long.toString(inputTwo, 7);
+                    String baseTwoEight = Long.toString(inputTwo, 8);
+                    String baseTwoNine = Long.toString(inputTwo, 9);
+                    String baseTwoTen = Long.toString(inputTwo, 10);
+                    String baseTwoEleven = Long.toString(inputTwo, 11);
+                    String baseTwoTwelve = Long.toString(inputTwo, 12);
+                    String baseTwoThirteen = Long.toString(inputTwo, 13);
+                    String baseTwoFourteen = Long.toString(inputTwo, 14);
+                    String baseTwoFifteen = Long.toString(inputTwo, 15);
+                    String baseTwoSixteen = Long.toString(inputTwo, 16);
+                    
+                    System.out.println("\nbase2: " + baseTwoTwo);
+                    System.out.println("base3: " + baseTwoThree);
+                    System.out.println("base4: " + baseTwoFour);
+                    System.out.println("base5: " + baseTwoFive);
+                    System.out.println("base6: " + baseTwoSix);
+                    System.out.println("base7: " + baseTwoSeven);
+                    System.out.println("base8: " + baseTwoEight);
+                    System.out.println("base9: " + baseTwoNine);
+                    System.out.println("base10: " + baseTwoTen);
+                    System.out.println("base11: " + baseTwoEleven);
+                    System.out.println("base12: " + baseTwoTwelve);
+                    System.out.println("base13: " + baseTwoThirteen);
+                    System.out.println("base14: " + baseTwoFourteen);
+                    System.out.println("base15: " + baseTwoFifteen);
+                    System.out.println("base16: " + baseTwoSixteen);
+                case 3:
+                    userInput.useRadix(3);
+                    System.out.println("\nEnter number to be converted: ");
+                    long inputThree = userInput.nextLong();
+                    String baseThreeTwo = Long.toString(inputThree, 2);
+                    String baseThreeThree = Long.toString(inputThree, 3);
+                    String baseThreeFour = Long.toString(inputThree, 4);
+                    String baseThreeFive = Long.toString(inputThree, 5);
+                    String baseThreeSix = Long.toString(inputThree, 6);
+                    String baseThreeSeven = Long.toString(inputThree, 7);
+                    String baseThreeEight = Long.toString(inputThree, 8);
+                    String baseThreeNine = Long.toString(inputThree, 9);
+                    String baseThreeTen = Long.toString(inputThree, 10);
+                    String baseThreeEleven = Long.toString(inputThree, 11);
+                    String baseThreeTwelve = Long.toString(inputThree, 12);
+                    String baseThreeThirteen = Long.toString(inputThree, 13);
+                    String baseThreeFourteen = Long.toString(inputThree, 14);
+                    String baseThreeFifteen = Long.toString(inputThree, 15);
+                    String baseThreeSixteen = Long.toString(inputThree, 16);
+                    
+                    System.out.println("\nbase2: " + baseThreeTwo);
+                    System.out.println("base3: " + baseThreeThree);
+                    System.out.println("base4: " + baseThreeFour);
+                    System.out.println("base5: " + baseThreeFive);
+                    System.out.println("base6: " + baseThreeSix);
+                    System.out.println("base7: " + baseThreeSeven);
+                    System.out.println("base8: " + baseThreeEight);
+                    System.out.println("base9: " + baseThreeNine);
+                    System.out.println("base10: " + baseThreeTen);
+                    System.out.println("base11: " + baseThreeEleven);
+                    System.out.println("base12: " + baseThreeTwelve);
+                    System.out.println("base13: " + baseThreeThirteen);
+                    System.out.println("base14: " + baseThreeFourteen);
+                    System.out.println("base15: " + baseThreeFifteen);
+                    System.out.println("base16: " + baseThreeSixteen);
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
             }
         }
     }
 }
-
-//stylised version
-/*
-import java.util.Scanner;
-public class RadixConverter {
-    public static void main(String[] args) {
-        
-        Scanner userInput = new Scanner(System.in);
-        int programState = 1;
-        
-        while (programState < 2) {
-            System.out.println("\nEnter input:");
-            
-            if (!userInput.hasNextLong()) {
-                String textInput = userInput.nextLine();
-                switch (textInput) {
-                    case "STOP":
-                        programState++;
-                        break;
-                    default:
-                        System.out.println("\nType STOP to close program.");
-                        break;
-                }
-            } else {
-                long numberInput = userInput.nextLong();
-            
-                System.out.println("\nEnter desired base:");
-                int radix = userInput.nextInt();
-        
-                switch (radix) {
-                    case 16:
-                        String decimalToHex = Long.toString(numberInput, 16);
-                        System.out.println("\nHexadecimal Result: " + decimalToHex);
-                        break;
-                    case 8:
-                        String decimalToOctal = Long.toString(numberInput, 8);
-                        System.out.println("\nOctal Result: " + decimalToOctal);
-                        break;
-                    case 2:
-                        String decimalToBinary = Long.toString(numberInput, 2);
-                        System.out.println("\nBinary Result: " + decimalToBinary);
-                        break;
-                    default:
-                        System.out.println("\nChoose between 2, 8, or 16 for now.");
-                }
-            }
-        }
-        try {
-            System.out.println("\nClosing program...");
-            Thread.sleep(1000);
-        } catch (InterruptedException e) { 
-            e.printStackTrace(); 
-        }
-    }
-}
-*/
