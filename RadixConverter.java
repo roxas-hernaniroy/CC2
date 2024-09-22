cb62ec132ad92bc31238ee97c4d5ee19952e5b26
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-09-22 12:11:11 +0800

    Update RadixConverter.java

--- RadixConverter.java
+++ RadixConverter.java
@@ -2,7 +2,7 @@
 //CITCS 1B-B
 
 import java.util.Scanner;
-public class RadixConverterII {
+public class RadixConverter {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         
