5452c9ff4cf1ac81948c6ecd3ad4418fd1dfa394
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-10-02 09:37:57 +0900

    Create DiscountCalculator.java

---
+++ DiscountCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class DiscountCalculator {
+    public static void main (String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        int purchaseTotal = 0;
+
+        System.out.println("Enter the total purchase amount: ");
+        purchaseTotal = userInput.nextInt();
+
+        if (purchaseTotal < 1000) {
+            System.out.println("Discount applied:\t0%");
+            System.out.println("Final price after discount:\t" + purchaseTotal);
+        }
+        else if (purchaseTotal >= 1000 && purchaseTotal <= 5000) {
+            System.out.println("Discount applied:\t5%");
+            double fivePercentDiscount = purchaseTotal * 0.05;
+            double finalPriceFivePercent = purchaseTotal - fivePercentDiscount;
+            System.out.println("Final price after discount\t" + );
+        }
+        else if (purchaseTotal >= 5001 && purchaseTotal <= 10000) {
+            System.out.println("Discount applied:\t10%");
+        }
+        else if (purchaseTotal > 10000) {
+
+        }
+    }
+}
