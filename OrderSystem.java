b6b7d6869d73bbf90ecd262f99b979f41bbc5740
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-10-05 09:59:52 +0800

    Update OrderSystem.java

--- OrderSystem.java
+++ OrderSystem.java
@@ -2,37 +2,52 @@ import java.util.Scanner;
 public class OrderSystem {
     public static void main(String[] args) {
 
-    Scanner userInput = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
 
-    int menuSelection = 5;
-    int quantitySelection = 0;
-    int totalCost = 0;
+        int menuSelection = 5;
+        int quantitySelection = 0;
+        int totalCost = 0;
 
-    System.out.println("MENU\n1. Burger\t- PHP 100\n2. Fries\t- PHP  50\n3. Soda\t- PHP  30\n4. Ice Cream\t- PHP  45\n5. Exit);
+        System.out.println("MENU\n1. Burger\t- PHP 100\n2. Fries\t- PHP  50\n3. Soda\t- PHP  30\n4. Ice Cream\t- PHP  45\n5. Exit);
     
-    System.out.print("Enter menu number: ");
-    menuSelection = userInput.nextInt();
+        System.out.print("Enter menu number: ");
+        menuSelection = userInput.nextInt();
     
-    if (menuSelection > 5 || menuSelection < 1) {
-        System.out.println("Invalid order.");
-    }
-    else {
-    System.out.print("Enter quantity: );
-    quantitySelection = userInput.nextInt();
+        if (menuSelection > 5 || menuSelection < 1) {
+            System.out.println("Invalid order.");
+            System.exit(0);
+        }
+        else if (menuSelection = 5) {
+            System.out.println("Exiting program."
+            System.exit(0);
+        }
+
+        System.out.print("Enter quantity: );
+        quantitySelection = userInput.nextInt();
         
         if (quantitySelection <= 0) {
             System.out.println("Invalid quantity.");
+            System.exit(0);
         }
-    }
-
-    System.out.println("You have ordered " + quantitySelection + " " + menuSelection + ".");
     
-    switch (menuSelection) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-    }
+        switch (menuSelection) {
+            case 1:
+                System.out.println("You have ordered " + quantitySelection + " burgers.");
+                totalCost = 100 * quantitySelection;
+                break:
+            case 2:
+                System.out.println("You have ordered " + quantitySelection + " fries.");
+                totalCost = 50 * quantitySelection;
+                break:
+            case 3:
+                System.out.println("You have ordered " + quantitySelection + " soda.");
+                totalCost = 30 * quantitySelection;
+                break;
+            case 4:
+                System.out.println("You have ordered " + quantitySelection + " ice cream.");
+                totalCost = 45 * quantitySelection;
+                break;
+        }
+        System.out.println("Total amount: " + totalCost);
     }
 }
