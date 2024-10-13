a914cd196e40890d01f65f0062cff4de4cd13202
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-10-13 11:19:56 +0800

    Create PasswordValidation.java

---
+++ PasswordValidation.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.regex.*;
+public class PasswordValidation {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        boolean loopCondition = true;
+        String userPass;
+        int passLength;
+        Pattern containsUppercase = Pattern.compile("[A-Z]");
+        Matcher uppercaseMatch;
+        boolean uppercaseTrue;
+        Pattern containsNumber = Pattern.compile("[0-9]");
+        Matcher numberMatch;
+        boolean numberTrue;
+
+        do { 
+            System.out.println("Enter password: ");
+            userPass = userInput.nextLine();
+            passLength = userPass.length();
+            uppercaseMatch = containsUppercase.matcher(userPass);
+            uppercaseTrue = uppercaseMatch.find();
+            numberMatch = containsNumber.matcher(userPass);
+            numberTrue = numberMatch.find();
+
+            if (passLength >= 8 && uppercaseTrue && numberTrue) {
+                loopCondition = false;
+                break;
+            }
+            else {
+                System.out.println("Password must contain at least 8 characters, including one uppercase letter and one number.");
+            }
+        } while (loopCondition);
+
+        System.out.println("Your password is valid.");
+        userInput.close();
+    }
+}
