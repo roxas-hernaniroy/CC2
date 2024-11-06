948d4b7278169b5ecd9741dcbc7a276f90fa85fd
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-11-06 08:45:54 +0800

    Create InventoryCalc.java

---
+++ InventoryCalc.java
@@ -0,0 +1,25 @@
+package javaapplication72;
+
+import java.util.Scanner;
+public class InventoryCalc {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        
+        int[] itemStock = new int[10];
+        int[] itemPrice = new int[10];
+        double invValue = 0;
+        
+        for (int invIndex = 0; invIndex <= 9; invIndex++){
+            System.out.println("Enter stock level of item " + (invIndex + 1) + ":\t");
+            itemStock[invIndex] = userInput.nextInt();
+            
+            System.out.println("Enter stock price of item " + (invIndex + 1) + ":\t");
+            itemPrice[invIndex] = userInput.nextInt();
+            
+            invValue = invValue + (itemStock[invIndex] * itemPrice[invIndex]);
+        }
+        
+        System.out.printf("Total inventory value:\t" + "&.2f", invValue);
+    }
+}
