686291e4877b586f01341d0d282c21ef332e7e73
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-11-19 09:59:33 +0800

    Create WeatherData.java

---
+++ WeatherData.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class WeatherData {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        int cities = 3;
+        int days = 7;
+        double[][] cityWeather =  new double[days][cities];
+
+    }
+}
