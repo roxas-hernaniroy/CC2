be1d14c577720a4763c26cb683ad2bc71340d4f4
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-11-20 08:47:37 +0800

    Update WeatherData.java

--- WeatherData.java
+++ WeatherData.java
@@ -2,21 +2,20 @@ import java.util.Scanner;
 public class WeatherData {
     public static void main (String[] args) {
         Scanner userInput = new Scanner(System.in);
-        
-       
-        double[][] weather = new double[2][5];
+        double[][] weather = new double[3][7];
         
         for (int i = 0; i < weather.length; i++) {
             for (int j = 0; j < weather[i].length; j++) {
-                System.out.println("Input weather of city " + (i + 1) + " for day " + (j + 1)+ ":");
-                weather[i][j] = userInput.nextInt();
+                System.out.println("Input temperature of city " + (i + 1) + " for day " + (j + 1)+ ":");
+                weather[i][j] = userInput.nextDouble();
             }
         }
         
         for (int i = 0; i < weather.length; i++) {
             for (int j = 0; j < weather[i].length; j++) {
-                System.out.println("City " + (i + 1) + " day " + (j + 1) + ": " + weather[i][j]);
+                System.out.print(weather[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
