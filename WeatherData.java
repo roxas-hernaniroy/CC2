3c30a2161cb6a8b5cf6b8097d9b1be5d07ce2ac8
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-11-21 19:45:09 +0800

    Update WeatherData.java

--- WeatherData.java
+++ WeatherData.java
@@ -23,8 +23,8 @@ public class WeatherData {
         }
 
         for (int resultRow = 0; resultRow < 3; resultRow++) {
-            System.out.println("The average temperature for city " + (resultRow + 1) + " is:" + average[resultRow]);
-            System.out.println("The highest temperature for city " + (resultRow + 1) + " is :" + highest[resultRow]);
+            System.out.printf("The average temperature for city %d is: %.2f\n", (resultRow + 1), average[resultRow]);
+            System.out.printf("The highest temperature for city %d is: %.2f\n", (resultRow + 1), highest[resultRow]);
         }
     }
 }
