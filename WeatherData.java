7ffde3198a4a075689c370af7dde400b5000e1c0
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-11-21 19:17:14 +0800

    Update WeatherData.java

--- WeatherData.java
+++ WeatherData.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 public class WeatherData {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        double[][] weather = new double[3][7];
+        double[][] temperatures = new double[3][7];
+        double[] highest = new double[3];
+        double[] average = new double[3];
+        double total = 0;
         
-        for (int i = 0; i < weather.length; i++) {
-            for (int j = 0; j < weather[i].length; j++) {
-                System.out.println("Input temperature of city " + (i + 1) + " for day " + (j + 1)+ ":");
-                weather[i][j] = userInput.nextDouble();
-                int total = 0;
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 7; column++) {
+                System.out.println("Enter temperature for city " + (row + 1) + " on day " + (column + 1) + ":");
+                temperatures[row][column] = userInput.nextDouble();
+                total += temperatures[row][column];
+                
+                if (temperatures[row][column] > highest[row]) {
+                    highest[row] = temperatures[row][column];
+                }
             }
+
+            average[row] = total / 7;
+            total = 0;
         }
-        
-        for (int i = 0; i < weather.length; i++) {
-            for (int j = 0; j < weather[i].length; j++) {
-                System.out.print(weather[i][j] + " ");
-            }
-            System.out.println();
+
+        for (int resultRow = 0; resultRow < 3; resultRow++) {
+            System.out.println("The average temperature for city " + (resultRow + 1) + " is:" + average[resultRow]);
+            System.out.println("The highest temperature for city " + (resultRow + 1) + " is :" + highest[resultRow]);
         }
     }
 }
