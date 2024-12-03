b0ba3d4b0d35a02c3f53ad1ede9dc31ac74bf3d5
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-12-04 07:27:21 +0800

    Update BrowserHistory.java

--- BrowserHistory.java
+++ BrowserHistory.java
@@ -1,26 +1,63 @@
+class Node {
+    int data;
+    Node previous;
+    Node next;
+
+    Node(int dataValue) {
+        data = dataValue;
+        previous = null;
+        next = null;
+    }
+}
+
+class Methods {
+    Node head = null;
+    Node tail = null;
+}
+
 class DoublyLinkedList {
     class Node {
-        int data;
-        Node prev;
+        int dataValue;
+        Node previous;
         Node next;
 
-        public Node (int data) {
-        this.data = data;
-        this.prev = null;
+        public Node (int dataValue) {
+        this.dataValue = dataValue;
+        this.previous = null;
         this.next = null;
         }
 
-        Node head = null;
-        Node tail = null;
+        Node headNode = null;
+        Node tailNode = null;
 
-        public void addNode() {
-            Node newNode = new Node(data);
+        public void goForward() {
+            Node currentNode = headNode;
+            while (currentNode != null) {
+                System.out.println(currentNode.dataValue)
+            }
+        }
+
+        public void goBackward() {
+        }
+
+        public void addNodeAtTail() {
+            Node temporary = new Node(data);
+            if (tail == null) {
+                head = temporary;
+                tail = temporary;
+            }
+            else {
+                tail.next = temporary;
+                temporary.prev = tail;
+                tail = temporary;
+            }
         }
     
         public void deleteNode() {
         }
 
         public void displayNode() {
+            
         }
     }
 }
