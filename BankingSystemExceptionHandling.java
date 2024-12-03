1dba5084f95bb5b6a11bcd908be65195959ca225
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-12-03 08:46:01 +0800

    Update BankingSystemExceptionHandling.java

--- BankingSystemExceptionHandling.java
+++ BankingSystemExceptionHandling.java
@@ -1,38 +1,41 @@
+//Roxas, Hernani Roy B.
+//CITCS 1B-B
 import java.util.Scanner;
 
 class InsufficientFundsException extends Exception {
-    public InsufficeinteFundsException
+    public InsufficientFundsException(String error) {
+        super(error);
+    }
 }
 
-public class BankingSystemExceptionHanding {
+public class BankingSystemExceptionHandling {
     public static void main(String[] args) {
         try {
             Scanner userInput = new Scanner(System.in);
-            double balance = (Math.random()* 1000) * (Math.random() * 100);
+            double balance = Math.random() * 1000000 + 1; //random balance for demonstration purposes
             
             while (true) {
                 System.out.printf("Welcome. Your current balance is %.2f.\n", balance);
-                System.out.print("Would you like to process a withdrawal? [Y/N]\n");
-                String userChoice = userInput.nextLine();
-            
-                if (userChoice.equalsIgnoreCase("y")) {
-                    System.out.print("Enter withdrawal amount:\n");
-                    double withdrawalAmount = userInput.nextDouble();
+                System.out.print("Enter withdrawal amount:\n");
+                double withdrawalAmount = userInput.nextDouble();
+                userInput.nextLine();
                     
-                    if (withdrawalAmount - balance < 0 || withdrawalAmount - balance > balance) {
-                        double error = balance / 0;
-                        break;
-                    }
-                    else {
-                        balance -= withdrawalAmount;
-                        System.out.printf("Amount of %.2f was withdrawn. Thank you for your patronage. Your new balance is %.2f.", withdrawalAmount, balance);
-                        break;
-                    }
+                if (balance - withdrawalAmount < 0) {
+                    userInput.close();
+                    throw new InsufficientFundsException("You have insufficient funds for this operation.");
                 }
+                else {
+                    balance -= withdrawalAmount;
+                    System.out.printf("Amount of %.2f was withdrawn. Thank you for your patronage. Your new balance is %.2f.", withdrawalAmount, balance);
+                    userInput.close();
+                    break;
+                }
+                
             }
         }
-        catch (Exception error) {
-            System.out.print("You have insufficient funds for this operation.\n");
+        catch (InsufficientFundsException error) {
+            System.out.println("Error detected.");
+            System.out.println(error.getMessage());
         }
     }
 }
