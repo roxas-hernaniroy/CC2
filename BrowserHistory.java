a7c65f288aa58295c8becf07bab952dca1cc968f
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-12-04 08:50:49 +0800

    Update BrowserHistory.java

--- BrowserHistory.java
+++ BrowserHistory.java
@@ -11,24 +11,29 @@ public class BrowserHistory {
                 current = "the home page";
             }
 
-            String pastPage;
+            int indexOfPastPage = 0;
 
             System.out.println("You are currently on " + current + "\n[n = new page | b = back | f = forward]");
             String operation = userInput.nextLine();
 
             switch (operation) {
                 case "n":
-                int indexOfPastPage = history.lastIndexOf(current);
+                indexOfPastPage = history.lastIndexOf(current);
                 System.out.println("Enter URL:");
                 current = userInput.nextLine();
                 history.addLast(current);
                 break;
 
                 case "b":
-
+                System.out.println(indexOfPastPage);
+                current = history.get(indexOfPastPage);
+                if (indexOfPastPage == 0) {
+                    current = "the home page";
+                }
                 break;
 
                 case "f":
+                break;
 
                 default:
                 return;
@@ -36,6 +41,182 @@ public class BrowserHistory {
         }
     }
 }
+
+/*class Node {
+    int data;
+    Node previous;
+    Node next;
+
+        public Node(int dataValue) {
+            this.data = dataValue;
+            this.previous = null;
+            this.next = null;
+        }
+}
+
+class DoublyLinkedList {
+    Node head;
+    Node tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    static int findLength(Node head) {
+        int count = 0;
+        for (Node currentNode = head; currentNode != null; currentNode = currentNode.next)
+            count++;
+        return count;
+    }
+
+    public void goBack() {
+        Node currentNode = tail;
+        while (currentNode != null) {
+            System.out.println(currentNode.data + " ");
+            currentNode = currentNode.previous;
+        }
+    }
+
+    public void goForward() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.println(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+    }
+
+    public void newPageAtEnd(int dataValue) {
+        Node temporaryNode = new Node(dataValue);
+        if (tail == null) {
+            head = temporaryNode;
+            tail = temporaryNode;
+        }
+        else {
+            tail.next = temporaryNode;
+            temporaryNode.previous = tail;
+            tail = temporaryNode;
+        }
+    }
+
+    public void deleteForwardHistory() {
+        if (tail == null) {
+            return;
+        }
+        
+        if (head == tail) {
+            head = null;
+            tail = null;
+            return;
+        }
+
+        Node temporaryNode = head;
+        tail = tail.previous;
+        tail.next = null;
+        temporaryNode.previous = null;
+    }
+
+    public void newPageAfterBack(int dataValue) {
+        Node temporaryNode = new Node(dataValue);
+
+    }
+
+    public void display() {
+        Node temporaryNode = head;
+        while (temporaryNode != null) {
+            System.out.println(temporaryNode.data + " ");
+            temporaryNode = temporaryNode.next;
+        }
+    }
+}
+
+public class BrowserHistory {
+    public static void main(String[] args) {
+        while (true) { 
+            System.out.println("Please choose operation:\n [n = new page | b = backward | f = forward]");
+            String operation = userInput.nextLine();
+            switch (operation) {
+                case "n":
+                
+                case "b":
+                
+                case "f":
+            }
+        }
+    }
+}*/
+
+/*class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+public class CircularDoublyLinkedList {
+    Node head = null;
+    Node tail = null;
+
+    public void traverseForward() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + "");
+            current = current.next;
+        }
+    }
+
+    public void traverseBackward() {
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + "");
+            current = current.prev;
+        }
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            newNode.next = head;
+        } else {
+            Node current = head;
+            while (current.next != head) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.next = head;
+        }
+    }
+
+    public void display() {
+        if (head != null) {
+            Node current = head;
+            do {
+                System.out.print(current.data + " ");
+                current = current.next;
+            } while (current != head);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        CircularDoublyLinkedList list = new CircularDoublyLinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.insert(50);
+        
+        System.out.println("Circular Linked List:");
+        list.display();
+    }
+}*/
 
 /*class Node {
     int data;
