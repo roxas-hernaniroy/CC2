c689b4d4a3697a354e18d266e60ac517f88a1ff4
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2024-12-06 21:37:42 +0800

    Update JavaDrawing.java

--- JavaDrawing.java
+++ JavaDrawing.java
@@ -1,91 +1,47 @@
-/*import java.awt.*;
-import javax.swing.*; //JOption import
-
-public class JavaDrawing extends Canvas {
-    
-    public void paint(Graphics graphics) {
-        graphics.setColor(Color.GREEN);
-        graphics.drawRect(50, 50 , 150, 100); //[x, y, width, height]
-        graphics.setColor(Color.BLUE);
-        graphics.fillRect(250, 50, 150, 100);
-        graphics.setColor(Color.RED);
-        graphics.drawOval(450, 50, 100, 100);
-        graphics.fillOval(600, 50, 100, 100);
-        graphics.setColor(Color.BLACK);
-        graphics.drawLine(50, 200, 700, 200); //[x, y, x, y]
-        graphics.drawLine(750, 50, 750, 150);
-        graphics.drawLine(800, 50, 800, 150);
-    }
-    
-    public static void main (String[] args) {
-        JFrame dialogueFrame = new JFrame("Java Drawing");
-        JavaDrawing canvas = new JavaDrawing();
-        dialogueFrame.setSize(500,500);
-        dialogueFrame.add(canvas);
-        dialogueFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        dialogueFrame.setVisible(true);
-    }
-}*/
-
 import java.awt.*;
 import javax.swing.*;
 
 public class JavaDrawing extends Canvas {
-    public void paint(Graphics graphics) {
-        graphics.setColor(Color.GRAY);
-        graphics.fillRect(0,0,500,500);
-        graphics.setColor(Color.DARK_GRAY);
-        graphics.fillOval(100, 100, 250, 250);
-        graphics.drawLine(100, 225, 100, 400);
-        graphics.drawLine(350, 225, 350, 400);
-        graphics.drawLine(100,400,150,225);
-        graphics.setColor(Color.RED);
-        graphics.fillOval(150, 200, 50, 50);
-        graphics.fillOval(250, 200, 50, 50);
-    }
+    public void paint(Graphics g) {
+        setBackground(Color.DARK_GRAY);
 
+        g.setColor(Color.GRAY);
+        g.fillRect(0, 350, 600, 300);
+
+
+        g.setColor(Color.WHITE);
+        g.fillOval(230, 200, 140, 200);
+        g.fillOval(235, 130, 130, 130);
+        g.drawLine(250, 250, 180, 320);
+        g.drawLine(350, 250, 430, 320);
+
+        g.setColor(Color.ORANGE);
+        int[] noseX = {300, 325, 300};
+        int[] noseY = {180, 190, 195};
+        g.fillPolygon(noseX, noseY, 3);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(265, 50, 70, 90);
+        g.fillRect(230, 130, 140, 10);
+        g.fillOval(270, 170, 10, 10);
+        g.fillOval(330, 170, 10, 10);
+        g.drawArc(300, 200, 20, 10, 0, -180);
+        g.fillOval(300, 250, 5, 5);
+        g.fillOval(300, 300, 5, 5);
+        g.fillOval(300, 350, 5, 5);
+
+        g.setColor(Color.WHITE);
+        g.fillRect(265, 110, 70, 10);
+
+    }
     public static void main (String[] args) {
-        JFrame dialogueFrame = new JFrame("Java Drawing");
-        JavaDrawing canvas = new JavaDrawing();
-        dialogueFrame.setSize(500,500);
-        dialogueFrame.add(canvas);
-        dialogueFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        dialogueFrame.setVisible(true);
+        JFrame dialogFrame = new JFrame("Snowman With A hat");
+        JavaDrawing canvasDrawing = new JavaDrawing();
+        canvasDrawing.setSize(600,600);
+        
+        dialogFrame.add(canvasDrawing);
+        dialogFrame.setSize(600,600);
+        dialogFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dialogFrame.setVisible(true);
     }
 }
-
---------------
-
-import java.awt.*;
-import javax.swing.*;
-
-public class JavaDrawing extends Canvas {
-    public void paint(Graphics graphics) {
-
-        setBackground(Color.GRAY);
-
-        graphics.setColor(Color.DARK_GRAY);
-        graphics.fillOval(100, 100, 250, 250);
-        graphics.drawLine(100, 100, 100, 220);
-        graphics.drawLine(350, 100, 350, 220);
-        int[] rightEarX = {100, 150, 100};
-        int[] rightEarY = {350, 300, 350};
-        int[] leftEarX = {350, 300, 350};
-        int[] leftEarY = {};
-        graphics.fillPolygon(rightEarX, rightEarY, 3);
-
-        graphics.setColor(Color.RED);
-        graphics.fillOval(150, 200, 50, 50);
-        graphics.fillOval(250, 200, 50, 50);
-    }
-
-    public static void main (String[] args) {
-        JFrame dialogueFrame = new JFrame("Java Drawing");
-        JavaDrawing canvas = new JavaDrawing();
-        dialogueFrame.setSize(500,500);
-        dialogueFrame.add(canvas);
-        dialogueFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        dialogueFrame.setVisible(true);
-    }
-}
-
