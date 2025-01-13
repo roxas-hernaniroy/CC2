fe5245075c4944e6ff13a95b87a9f25862318ad3
Hernani Roy Roxas <roxas.hernaniroy@gmail.com>
2025-01-13 19:24:53 +0800

    Update and rename Swingers.java to Swing.java

--- Swing.java
+++ Swing.java
@@ -1,11 +1,16 @@
 package javaapplication28;
 import java.util.Scanner;
+import java.awt.*;
 import javax.swing.*;
 
-public class Swingers {
+public class Swing {
     public static void main(String[] args) {
         //interface
         JFrame userWindow = new JFrame("Reserve");
+        
+        JTextField textInput = new JTextField();
+        textInput.setBounds(100, 100, 400, 50);
+        //userWindow.add(textInput);
         
         JButton confirmButton = new JButton("Confirm");
         confirmButton.setBounds(175, 500, 250, 50);
@@ -16,18 +21,30 @@ public class Swingers {
         userWindow.setLayout(null);
         userWindow.setVisible(true);
         
+        JLabel label = new JLabel();
+        label.setText("aaaaaaaaaaaaaaaaaaaaaaa");
+        JPanel panel = new JPanel();
+        
+        panel.add(label);
+        userWindow.add(panel);
+        userWindow.show();
+        
         //input
         Scanner userInput = new Scanner(System.in);
         
         //storage
         String rooms[] = new String[10];
-        
         String reserveRequest = null;
+        
+        System.out.println("question");
+        reserveRequest = userInput.nextLine();
+        
         int roomSelection = 0;
         
         if (rooms[roomSelection].isEmpty()) {
             rooms[roomSelection] = reserveRequest;
             System.out.println("result a");
+            
         } else {
             System.out.println("result b");
         }
